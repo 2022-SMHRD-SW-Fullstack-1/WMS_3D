eb2ef34db1692e48a6ab2c189ccd78c02145b900
SUNGJUN1234 <chotjdwns654@naver.com>
2023-01-12 20:23:48 +0900

    창고페이지 기초 틀 생성

--- wms_front/src/pages/warehouse/Warehouse.js
+++ wms_front/src/pages/warehouse/Warehouse.js
@@ -7,7 +7,19 @@ const Warehouse = () => {
     <div className='wh_top_div'>
       <SideBar/>
       <div >
-          창고입니다
+        <div className='wh_management_div'>
+          창고관리/선반관리
+        </div>
+        <div className='wh_button_div'>
+          <button>수정</button> 검색 추가
+        </div>
+        <div className='wh_main_div'>
+          <div className = 'wh_graph'></div>
+          <div className='wh_mini_div'>
+            <div  className = 'wh_mini_diagram'></div>
+            <div className='wh_mini_graph'></div>
+          </div>
+        </div>
       </div>
     </div>
   )
