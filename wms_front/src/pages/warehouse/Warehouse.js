2c47ceff046ee51741ed5aa69d3c34c8138b65bd
고연주 <112370767+yeonjuko@users.noreply.github.com>
2023-01-13 10:18:51 +0900

    중간 커밋

--- wms_front/src/pages/warehouse/Warehouse.js
+++ wms_front/src/pages/warehouse/Warehouse.js
@@ -1,16 +1,14 @@
-import React from 'react'
-import '../../css/Warehouse.css'
-import SideBar from '../../components/SideBar'
+import React from "react";
+import "../../css/Warehouse.css";
+import SideBar from "../../components/SideBar";
 
 const Warehouse = () => {
   return (
-    <div className='wh_top_div'>
-      <SideBar/>
-      <div >
-          창고입니다
-      </div>
+    <div className="wh_top_div">
+      <SideBar />
+      <div className="wh_ex_div">창고입니다</div>
     </div>
-  )
-}
+  );
+};
 
-export default Warehouse
+export default Warehouse;
