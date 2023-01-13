280e1f4b9bc114a5c75f088dacaee2b42d3cb2fb
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2023-01-13 17:07:06 +0900

    sidbar6

---
+++ wms_front/src/components/SideBarMenu.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Warehouse from "../pages/warehouse/Warehouse";
+import WarehouseIcon from "@mui/icons-material/Warehouse";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import OutputIcon from "@mui/icons-material/Output";
+import WebStoriesIcon from "@mui/icons-material/WebStories";
+
+export const SideBarMenu = [
+  {
+    title: "창고",
+    icon: <WarehouseIcon />,
+    link: "warehouse/",
+  },
+
+  {
+    title: "입고",
+    icon: <InventoryIcon />,
+    link: "input/",
+  },
+
+  {
+    title: "출고",
+    icon: <OutputIcon />,
+    link: "output/",
+  },
+
+  {
+    title: "재고",
+    icon: <WebStoriesIcon />,
+    link: "stock/",
+  },
+];
