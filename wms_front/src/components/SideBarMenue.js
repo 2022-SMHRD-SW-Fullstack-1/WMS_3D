463310185e43703a1ff60734cc9311a06a3b1fd9
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2023-01-13 11:07:24 +0900

    sidebar2

--- wms_front/src/components/SideBarMenue.js
+++ wms_front/src/components/SideBarMenue.js
@@ -1,15 +1,44 @@
 import React from "react";
+import Warehouse from "../pages/warehouse/Warehouse";
+import WarehouseIcon from '@mui/icons-material/Warehouse';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import OutputIcon from '@mui/icons-material/Output';
+import WebStoriesIcon from '@mui/icons-material/WebStories';
 
-function SideBarMenue({ menu, isActive }){
-        return isActive === true ? (
-                <div className="sidebar-menue active">
-                  <p>{menu.name}</p>
-                </div>
-              ) : (
-                <div className="sidebar-menue ">
-                  <p>{menu.name}</p>
-                </div>
-              );
-            }
 
-export default SideBarMenue;
+export const SideBarMenue = [
+{
+title: "창고",
+icon: <WarehouseIcon />,
+link: "warehouse/"
+},
+
+{
+title: "입고",
+icon: <InventoryIcon/>,
+link: "input/"
+},
+
+{
+title: "출고",
+icon: <OutputIcon />,
+link: "output/"
+},
+
+{
+title: "재고",
+icon: <WebStoriesIcon />,
+link: "stock/"
+},
+];
+
+export const SideBarMyPage = [
+  {
+    icon: <WarehouseIcon />,
+    link: "warehouse/"
+    },
+  {
+    icon: <WarehouseIcon />,
+    link: "warehouse/"
+    },
+];
