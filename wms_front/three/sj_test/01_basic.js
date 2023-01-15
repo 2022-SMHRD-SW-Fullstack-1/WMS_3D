9e2934202e4eedaff026559e298b1e3a0b891c6c
chotj <chotjdwns654@naver.com>
2023-01-15 09:38:44 +0900

    선반 클릭 생성 기능

--- wms_front/three/sj_test/01_basic.js
+++ wms_front/three/sj_test/01_basic.js
@@ -6,6 +6,11 @@ import { OrbitControls } from "../examples/jsm/controls/OrbitControls.js"
 // let width = Number(prompt("창고의 너비를 입력하세요"))
 // let length = Number(prompt("창고의 길이를 입력하세요"))
 
+
+const shelf_width = Number(prompt("선반의 너비를 입력하세요"))
+const shelf_length = Number(prompt("선반의 길이를 입력하세요"))
+const shelf_floor = Number(prompt("선반의 층을 입력하세요"))
+
 class App{
     constructor() {
         const divContainer = document.querySelector("#webgl-container");
@@ -59,8 +64,21 @@ class App{
     // 파란색 정육면체 mesh 생성
     _setupModel(){
         
-        const watehouse_x = 10;
-        const watehouse_y = 20;
+        this._createBoard();
+        
+       
+
+
+    }
+
+    _createBoard(){
+        
+        // 창고 data
+        const watehouse_x = 30;
+        const watehouse_y = 60;
+        //
+
+
         const wareHouse = new THREE.Object3D();
         const planeGeometry = new THREE.PlaneGeometry(watehouse_x,watehouse_y,watehouse_x,watehouse_y)
     
@@ -91,19 +109,41 @@ class App{
  
          this._scene.add(mesh);
 
+         wareHouseMesh.name = 'ground'
+
+         this._warehouse = wareHouse
+
+         this._createShelf();
+    }
+
+
+    _createShelf(){
+
+        // 선반 data
+        // const shelf_width = 1;
+        // const shelf_length = 7;
+        // const shelf_floor = 4;
+        const rotation = true;
+        const meshName = "A선반"
+        //
         
-        //=====================================================================
+
+
+
+         //=====================================================================
         // 하이라이트 모양과 위치 잡기 
-        wareHouseMesh.name = 'ground'
 
         const highlightMesh = new THREE.Mesh(
-            new THREE.PlaneGeometry(1,1),
+            new THREE.PlaneGeometry(shelf_width,shelf_length),
             new THREE.MeshBasicMaterial({
                 side : THREE.DoubleSide
             })
         )
         highlightMesh.position.set(0.5,0,0.5)
         highlightMesh.rotation.x = -Math.PI/2;
+        if(rotation==true){
+            highlightMesh.rotation.z = -Math.PI/2;
+        }
 
         // 마우스 호버 이벤트 넣기
         this._scene.add(highlightMesh)
@@ -124,6 +164,18 @@ class App{
                 if(intersect.object.name === 'ground'){
                     const highlightPos = new THREE.Vector3().copy(intersect.point).floor().addScalar(0.5);
                     highlightMesh.position.set(highlightPos.x,0,highlightPos.z)
+                
+                
+                    const objectExist = objects.find(function(object){
+                        return(object.position.x === highlightMesh.position.x)
+                        && (object.position.z === highlightMesh.position.z)
+                    })
+                    if(!objectExist){
+                        highlightMesh.material.color.setHex(0xFFFFFF)
+                    }else{
+                        highlightMesh.material.color.setHex(0xFF0000)
+                    }
+                
                 }
             })
         })
@@ -134,37 +186,76 @@ class App{
         
         //------------------------
         // 클릭한 곳에 선반 생성
-        const sphereMesh = new THREE.Mesh(
-            new THREE.SphereGeometry(0.4,4,2),
-            new THREE.MeshBasicMaterial({
-                wireframe : true,
-                color : 0xffEA00
-            })
-        )
+        const objects = [];
+
+        const group2 = new THREE.Group();
+        // 기본 바 생성
+        const shelfBarGeometry = new THREE.CylinderGeometry(0.03,0.03,shelf_floor-1+0.2)
+        const shelfBarMaterial = new THREE.MeshPhongMaterial({
+            color : 0xffffff, emissive : 0x112244, flatShading:true
+        })
+        // 기본 판 생성
+        const shelfFloorGeometry = new THREE.BoxGeometry(shelf_width,0.05,shelf_length)
+        const shelfFloorMaterial = new THREE.MeshPhongMaterial({
+            color : 0xffffff, emissive : 0x112244, flatShading:true
+        })
+
+        for(let i=0;i<4;i++){
+            // 바생성
+            const shelfBarMesh =new THREE.Mesh(shelfBarGeometry,shelfBarMaterial);
+            let x = 1;
+            let z = 1;
+            if(i==1){
+                z=-1;
+            }else if(i==2){
+                x=-1;
+                z=-1;
+            }else if(i==3){
+                x=-1;
+            }
+            shelfBarMesh.position.x = shelf_width/2*x;
+            shelfBarMesh.position.y = 1/2*(shelf_floor-2)+(0.6);
+            shelfBarMesh.position.z = 1/2*z*shelf_length;
+            group2.add(shelfBarMesh);
+        }
+        for(let i=0;i<shelf_floor;i++){
+            // 판 생성
+            const shelfFloorMesh = new THREE.Mesh(shelfFloorGeometry,shelfFloorMaterial)
+            shelfFloorMesh.position.y = (i*1)+0.1;
+            group2.add(shelfFloorMesh);
+        }
+        if(rotation==true){
+            group2.rotation.y =-Math.PI/2;
+        }
+        
         window.addEventListener('mousedown',function(){
-            intersects.forEach(function(intersect){
-                if(intersect.object.name === 'ground'){
-                    const sphereClone = sphereMesh.clone();
-                    sphereClone.position.copy(highlightMesh.position);
-                    console.log(highlightMesh.position)
-                    hover_scene.add(sphereClone);
-                }
+            const objectExist = objects.find(function(object){
+                return(object.position.x === highlightMesh.position.x)
+                && (object.position.z === highlightMesh.position.z)
             })
+            
+            if(!objectExist){
+                intersects.forEach(function(intersect){
+                    if(intersect.object.name === 'ground'){
+                        const shelfClone = group2.clone();
+                        shelfClone.position.copy(highlightMesh.position);
+                        console.log(highlightMesh.position)
+                        hover_scene.add(shelfClone);
+                        objects.push(shelfClone)
+                        group2.name = meshName;
+                    }
+                })
+            }
             console.log(hover_scene.children.length)
         })
 
         //------------------------
 
 
-
-
- 
-    
-        this._warehouse = wareHouse
-        // this._createShelfs(watehouse_x,watehouse_y);
-
-
     }
+
+
+
 
     // 창의 크기가 변경될때 발생하는 이벤트
     resize(){
@@ -195,7 +286,3 @@ class App{
 window.onload = function(){
     new App();
 }
-
-
-
-
