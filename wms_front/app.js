fa0d6389ae13e28cb9fc1558569e766b7fd2bf08
SUNGJUN1234 <chotjdwns654@naver.com>
2023-01-17 13:39:39 +0900

    창고페이지 DB와 연동

--- wms_front/app.js
+++ wms_front/app.js
@@ -59,6 +59,33 @@ app.get("/output", (req, res) => {
   res.sendFile(__dirname + "/views/html/stock/output.html");
 });
 
+// 창고 관리 페이지
+app.get("/warehouse", (req, res) => {
+  mdbConn
+    .getWarehouseList()
+    .then((rows) => {
+      // console.log(rows);
+      res.render(
+        "views/html/warehouse/warehouse.ejs",
+        {
+          data: rows,
+        },
+        function (err, html) {
+          if (err) {
+            console.log(err);
+          }
+          console.log(rows);
+          res.end(html);
+        }
+      );
+    })
+    .catch((errMsg) => {
+      //   console.log(errMsg);
+      err;
+    });
+
+});
+
 app.post("/outputForm", (req, res) => {
   // console.log(req.body.pw);
   // console.log(req.body);
