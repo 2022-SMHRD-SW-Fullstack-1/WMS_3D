f85f57902380a4281425596c9a7f78a2687bb47b
SUNGJUN1234 <chotjdwns654@naver.com>
2023-01-31 17:41:08 +0900

    재고 위치 확인 기능

--- wms_front/views/js/stock/stock.js
+++ wms_front/views/js/stock/stock.js
@@ -1,12 +1,52 @@
 function goToWarehouse(e) {
-    console.log(e.wh_num); // 클릭한 창고 번호
-    const num = { num: e.wh_num };
-    let url = "/viewWarehouse";
-  
-    let form = document.createElement("form");
-    form.setAttribute("method", "post");
-    form.setAttribute("action", url);
-    document.characterSet = "utf-8";
+  console.log(e.wh_num); // 클릭한 창고 번호
+  const num = { num: e.wh_num };
+  let url = "/viewWarehouse";
+
+  let form = document.createElement("form");
+  form.setAttribute("method", "post");
+  form.setAttribute("action", url);
+  document.characterSet = "utf-8";
+  for (let key in num) {
+    let hiddenField = document.createElement("input");
+    hiddenField.setAttribute("type", "hidden");
+    hiddenField.setAttribute("name", key);
+    hiddenField.setAttribute("value", num[key]);
+    form.appendChild(hiddenField);
+  }
+  document.body.appendChild(form);
+  form.submit();
+
+  localStorage.setItem("clicked_stock_num", e.stock_num);
+}
+
+let checked_arr = [0];
+let result;
+
+function isChecked(e) {
+  if (checked_arr.includes(e)) {
+    let arrayRemove = (arr, value) => {
+      return arr.filter((ele) => {
+        return ele != value;
+      });
+    };
+    checked_arr = arrayRemove(checked_arr, e);
+  } else {
+    checked_arr.push(e);
+  }
+  console.log(checked_arr);
+}
+
+function stockOutput() {
+  let worker = prompt("판매처를 입력해주세요");
+
+  let url = "/stockOutput";
+  let form = document.createElement("form");
+  form.setAttribute("method", "post");
+  form.setAttribute("action", url);
+  document.characterSet = "utf-8";
+  for (let i = 0; i < checked_arr.length; i++) {
+    let num = { num: checked_arr[i], worker: worker };
     for (let key in num) {
       let hiddenField = document.createElement("input");
       hiddenField.setAttribute("type", "hidden");
@@ -14,59 +54,8 @@ function goToWarehouse(e) {
       hiddenField.setAttribute("value", num[key]);
       form.appendChild(hiddenField);
     }
-    document.body.appendChild(form);
-    form.submit();
-
-    localStorage.setItem("clicked_stock_num",e.stock_num)
-  }
-
-
-let checked_arr = [0]
-let result;
-
-
-function isChecked(e){
-  
-  if(checked_arr.includes(e)){
-    
-    let arrayRemove = (arr, value) => {
-      return arr.filter((ele) => {
-          return ele != value;
-      });
-    };
-    checked_arr = arrayRemove(checked_arr,e)
-
-  }else{
-    checked_arr.push(e)
-  }
-  console.log(checked_arr)
-
-}
-
-function stockOutput() {
-  let worker = prompt("판매처를 입력해주세요")
-
-
-  let url = "/stockOutput"
-  let form = document.createElement('form');
-  form.setAttribute('method','post')
-  form.setAttribute('action',url)
-  document.characterSet = "utf-8";
-  for(let i =0; i<checked_arr.length;i++){
-    let num = {num: checked_arr[i],worker: worker}
-    for(let key in num){
-      let hiddenField = document.createElement('input')
-      hiddenField.setAttribute('type','hidden');
-      hiddenField.setAttribute('name',key)
-      hiddenField.setAttribute('value',num[key]);
-      form.appendChild(hiddenField);
-    }
   }
 
   document.body.appendChild(form);
-    form.submit();
-
-
-
-
+  form.submit();
 }
