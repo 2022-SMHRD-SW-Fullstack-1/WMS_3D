0a5f6c7c956fd5ad5c772993544744bed7e8b49d
SUNGJUN1234 <chotjdwns654@naver.com>
2023-02-01 11:29:59 +0900

    재고 위치 오류 해결

--- wms_front/mariaDBConn.js
+++ wms_front/mariaDBConn.js
@@ -120,7 +120,7 @@ async function GetWork_HistoryList() {
     conn = await pool.getConnection();
     conn.query("USE wms");
     rows = await conn.query(
-      "SELECT wk.work_name,wkr.worker_name,st.stock_num,st.stock_name,st.stock_info,wk.from_position,wk.to_position,date_FORMAT(wk.replace_date,'%y년 %m월 %d일 %H:%i') replace_date FROM tbl_stock st left join tbl_work wk on st.stock_num = wk.stock_num left join tbl_worker wkr on wk.worker_num = wkr.worker_num where wk.work_name is not null AND wkr.worker_name is not null"
+      "SELECT wk.work_name,wkr.worker_name,st.stock_num,st.stock_name,st.stock_info,wk.from_position,wk.to_position,date_FORMAT(wk.replace_date,'%y년 %m월 %d일%H:%i') replace_date FROM tbl_stock st left join tbl_work wk on st.stock_num = wk.stock_num left join tbl_worker wkr on wk.worker_num = wkr.worker_num where wk.work_name is not null AND wkr.worker_name is not NULL ORDER BY replace_date"
     );
   } catch (err) {
     throw err;
@@ -137,7 +137,7 @@ async function GetStockList() {
     conn = await pool.getConnection();
     conn.query("USE wms");
     rows = await conn.query(
-      "SELECT st.stock_num, st.stock_name, st.stock_info, st.buy_com, wh.wh_name, date_FORMAT(st.exp_dt,'%y년 %m월 %d일') exp_dt, wh.wh_num FROM tbl_stock st left join tbl_warehouse wh ON st.com_num = wh.com_num WHERE st.output_date IS NULL GROUP BY st.stock_num"
+      "SELECT st.stock_num, st.stock_name, st.stock_info, st.buy_com, wh.wh_name, date_FORMAT(st.exp_dt,'%y년 %m월 %d일') exp_dt, wh.wh_num FROM tbl_stock st LEFT JOIN tbl_shelf s ON st.shelf_num = s.shelf_num LEFT JOIN tbl_warehouse wh ON s.wh_num = wh.wh_num WHERE st.output_date IS NULL AND wh.wh_num IS NOT null GROUP BY st.stock_num"
     );
   } catch (err) {
     throw err;
