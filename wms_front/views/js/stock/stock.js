8ad580cb4577043c70c80c365bcd0976358f72fd
SUNGJUN1234 <chotjdwns654@naver.com>
2023-02-01 15:32:09 +0900

    출고 기능 완성

--- wms_front/views/js/stock/stock.js
+++ wms_front/views/js/stock/stock.js
@@ -19,3 +19,55 @@ function goToWarehouse(e) {
 
     localStorage.setItem("clicked_stock_num",e.stock_num)
   }
+
+
+let checked_arr = []
+let result;
+
+
+function isChecked(e){
+  
+  if(checked_arr.includes(e)){
+    
+    let arrayRemove = (arr, value) => {
+      return arr.filter((ele) => {
+          return ele != value;
+      });
+    };
+    checked_arr = arrayRemove(checked_arr,e)
+
+  }else{
+    checked_arr.push(e)
+  }
+  console.log(checked_arr)
+
+}
+
+function stockOutput() {
+  let worker = prompt("판매처를 입력해주세요")
+
+  
+
+  let url = "/stockOutput"
+  let form = document.createElement('form');
+  form.setAttribute('method','post')
+  form.setAttribute('action',url)
+  document.characterSet = "utf-8";
+  for(let i =0; i<checked_arr.length;i++){
+    let num = {num: checked_arr[i],worker: worker}
+    for(let key in num){
+      let hiddenField = document.createElement('input')
+      hiddenField.setAttribute('type','hidden');
+      hiddenField.setAttribute('name',key)
+      hiddenField.setAttribute('value',num[key]);
+      form.appendChild(hiddenField);
+    }
+  }
+
+  document.body.appendChild(form);
+    form.submit();
+
+
+
+
+}
